e5c50e13a3da579b7a13607eec3c7e9483c26d29
Vadim <angryvadik@gmail.com>
2023-04-21 21:55:22 +0200

    refactor: extract logic

--- server/src/ws.rs
+++ server/src/ws.rs
@@ -2,44 +2,39 @@ use axum::extract::ws::{Message, WebSocket};
 use serde::{Deserialize, Serialize};
 use tokio::{select, sync::mpsc};
 
-use crate::{state::Client, AssignmentsType, ClientsType, ServerState};
+use crate::{
+    state::{Client, MessageEntry, MessageType, MessagesStore},
+    Assignments, Clients,
+};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum ClientMessage {
     Message {
         message: String,
-        room: String,
+        to: String,
     },
     Image {
         bytes: Vec<u8>,
-        message: String,
-        room: String,
+        message: Option<String>,
+        to: String,
+    },
+    Disconnect,
+    Read {
+        to: String,
+        id: String,
     },
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum ServerMessage {
-    Open {
-        id: String,
-        daddy: Option<String>,
-    },
-    Message {
-        message: String,
-        room: String,
-        from: String,
-    },
-    Image {
-        bytes: Vec<u8>,
-        message: String,
-        room: String,
-    },
-    Switch {
-        id: String,
-    },
+    Open { id: String, daddy: Option<String> },
+    Message(MessageEntry),
+    Switch { id: Option<String> },
     Update,
     Disconnect,
+    SendResult { id: String },
 }
 
 fn process_msg(msg: &Message) -> anyhow::Result<ClientMessage> {
@@ -50,165 +45,189 @@ fn process_msg(msg: &Message) -> anyhow::Result<ClientMessage> {
     let cl: ClientMessage = serde_json::from_str(msg.to_text()?)?;
     Ok(cl)
 }
-async fn get_some_help(state: AssignmentsType, my_id: &String) -> Option<String> {
-    match state
-        .lock()
+
+struct Connection {
+    socket: WebSocket,
+    id: String,
+}
+
+async fn send_message(socket: &mut WebSocket, msg: &ServerMessage) -> Result<(), axum::Error> {
+    socket
+        .send(Message::Text(
+            serde_json::to_string(msg).expect("always to convert"),
+        ))
         .await
-        .iter_mut()
-        .min_by_key(|(_, val)| val.len())
-    {
-        Some((id, clients)) => {
-            clients.push(my_id.clone());
-            Some(id.clone())
-        }
-        None => None,
+}
+
+impl Connection {
+    pub fn new(socket: WebSocket) -> Connection {
+        let id = uuid::Uuid::new_v4().to_string();
+        Connection { socket, id }
+    }
+    pub fn from_id(socket: WebSocket, id: String) -> Connection {
+        Connection { socket, id }
+    }
+    pub async fn _send(self) -> Result<(), axum::Error> {
+        let mut socket = self.socket;
+        socket.send(Message::Text("text".to_string())).await
+    }
+    pub async fn _disconnect(self) {
+        self.socket.close().await.unwrap();
     }
 }
 
 pub async fn handle_socket(
-    mut socket: WebSocket,
-    state: ClientsType,
-    assignments: AssignmentsType,
+    socket: WebSocket,
+    clients: Clients,
+    assignments: Assignments,
+    msg_store: MessagesStore,
     id: Option<String>,
     is_support: Option<bool>,
 ) {
+    //TODO: graceful socket disconnect
     let (tx, mut rx) = mpsc::channel(100);
-    let connection_id = match id {
+    let mut connection = match id {
         Some(id) => {
-            match state.lock().await.get_mut(&id) {
+            match clients.clients.lock().await.get_mut(&id) {
                 Some(client) => {
                     client.sender = Some(tx.clone());
                 }
                 //Id is fake so get him out
                 None => return,
             };
-            id
+            Connection::from_id(socket, id)
         }
         None => {
-            let id = uuid::Uuid::new_v4().to_string();
             //WARN: Needs refactoring
+            let connection = Connection::new(socket);
             if is_support.is_none() {
                 //TODO: await for help. Blocking?
-                match get_some_help(assignments.clone(), &id).await {
-                    Some(papa_id) => {
-                        let mut state = state.lock().await;
-                        let sender = state.get_mut(&papa_id).unwrap();
-                        sender
-                            .sender
-                            .clone()
-                            .unwrap()
-                            .send(ServerMessage::Update)
-                            .await
-                            .unwrap();
-                        state.insert(
-                            id.clone(),
-                            Client {
-                                sender: Some(tx.clone()),
-                                daddy: Some(papa_id),
-                            },
-                        );
-                    }
-                    None => return,
-                }
-            }
-            id
-        }
-    };
-    match is_support {
-        Some(_) => {
-            state.lock().await.insert(
-                connection_id.clone(),
-                Client {
-                    sender: Some(tx.clone()),
-                    daddy: None,
-                },
-            );
-            let mut assginments = assignments.lock().await;
-            assginments.insert(connection_id.clone(), vec![]);
-        }
-        None => (),
-    }
-
-    socket
-        .send(Message::Text(
-            serde_json::to_string(&ServerMessage::Open {
-                id: connection_id.clone(),
-                daddy: {
-                    state
-                        .lock()
-                        .await
-                        .get(&connection_id)
-                        .expect("client to exist")
-                        .daddy
-                        .clone()
-                },
-            })
-            .unwrap(),
-        ))
-        .await
-        .unwrap();
-    let handle_disconnect = || async {
-        let mut clients = state.lock().await;
-        let daddy_id = &clients.get(&connection_id).expect("client to exist").daddy;
-        match daddy_id {
-            Some(dad_id) => {
-                //notify daddy that i leave
-                if let Some(daddy) = clients.get(dad_id) {
-                    daddy
-                        .sender
-                        .clone()
-                        .expect("daddy to listen")
-                        .send(ServerMessage::Update)
+                if let Some(papa_id) = assignments.add_client(connection.id.clone()).await {
+                    clients
+                        .send_message(papa_id.clone(), ServerMessage::Update)
                         .await
                         .unwrap();
-
-                    //remove myself
-                    let mut assignments = assignments.lock().await;
-                    let daddys_clients = assignments.get(dad_id.as_str()).expect("daddy exists");
-                    let filtered = daddys_clients
-                        .iter()
-                        .filter(|ids| ids.as_str() != connection_id.as_str())
-                        .map(|ids| ids.clone())
-                        .collect();
-                    assignments.insert(dad_id.clone(), filtered);
+                    clients
+                        .add(
+                            connection.id.clone(),
+                            Client {
+                                sender: Some(tx.clone()),
+                                is_support: false,
+                                is_online: true,
+                                daddy: Some(papa_id),
+                            },
+                        )
+                        .await;
+                } else {
+                    clients
+                        .add(
+                            connection.id.clone(),
+                            Client {
+                                sender: Some(tx.clone()),
+                                is_support: false,
+                                is_online: true,
+                                daddy: None,
+                            },
+                        )
+                        .await;
                 }
             }
-            None => {
-                let mut assignments = assignments.lock().await;
-                let clients_ids = assignments.get(&connection_id).expect("to exist").clone();
-                assignments.remove(&connection_id);
-                for client_id in clients_ids {
-                    match assignments.iter_mut().min_by_key(|(_, val)| val.len()) {
-                        Some((new_dad_id, new_dad_clients)) => {
-                            clients
-                                .get(&client_id)
-                                .expect("to exist")
-                                .sender
-                                .clone()
-                                .unwrap()
-                                .send(ServerMessage::Switch {
-                                    id: new_dad_id.clone(),
-                                })
-                                .await
-                                .unwrap();
-                            new_dad_clients.push(client_id.clone());
-                            clients
-                                .get(new_dad_id)
-                                .expect("to exist")
-                                .sender
-                                .clone()
-                                .unwrap()
-                                .send(ServerMessage::Update)
-                                .await
-                                .unwrap();
-                            ()
-                        }
-                        None => (),
-                    };
+            connection
+        }
+    };
+    if let Some(_) = is_support {
+        clients
+            .add(
+                connection.id.clone(),
+                Client {
+                    is_support: true,
+                    sender: Some(tx.clone()),
+                    is_online: true,
+                    daddy: None,
+                },
+            )
+            .await;
+        assignments.add_support(connection.id.clone()).await;
+        let assignments = assignments.assignments.lock().await;
+        let orphans = assignments.get(&connection.id).expect("to exist");
+        for orphan in orphans {
+            clients
+                .send_message(
+                    orphan.clone(),
+                    ServerMessage::Switch {
+                        id: Some(connection.id.clone()),
+                    },
+                )
+                .await
+                .unwrap();
+        }
+    }
+    let socket = &mut connection.socket;
+
+    send_message(
+        socket,
+        &ServerMessage::Open {
+            id: connection.id.clone(),
+            daddy: {
+                clients
+                    .clients
+                    .lock()
+                    .await
+                    .get(&connection.id)
+                    .expect("client to exist")
+                    .daddy
+                    .clone()
+            },
+        },
+    )
+    .await
+    .unwrap();
+    let handle_disconnect = || async {
+        let cl = clients.clients.lock().await;
+        let me = cl.get(&connection.id).expect("to exist").clone();
+        drop(cl);
+        if me.is_support {
+            let orphans = assignments.remove_support(&connection.id).await;
+            for client in orphans {
+                let daddy = assignments.add_client(client.clone()).await;
+                match daddy {
+                    Some(daddy) => {
+                        clients
+                            .send_message(daddy.clone(), ServerMessage::Update)
+                            .await
+                            .unwrap();
+                        clients
+                            .send_message(client, ServerMessage::Switch { id: Some(daddy) })
+                            .await
+                            .unwrap();
+                    }
+                    //TODO: now my client is orphan
+                    None => {
+                        clients
+                            .send_message(client, ServerMessage::Switch { id: None })
+                            .await
+                            .unwrap();
+                    }
+                }
+            }
+        } else {
+            match &me.daddy {
+                Some(dad_id) => {
+                    //remove myself
+                    assignments.remove_client(&dad_id, &connection.id).await;
+                    //notify daddy that i leave
+                    clients
+                        .send_message(dad_id.clone(), ServerMessage::Update)
+                        .await
+                        .unwrap();
+                }
+                None => {
+                    println!("removing");
                 }
             }
         }
-        clients.remove(&connection_id);
+        clients.remove(&connection.id).await;
+        assignments.remove_from_wait_list(&connection.id).await;
         println!("Client disconnected");
     };
 
@@ -230,28 +249,28 @@ pub async fn handle_socket(
                     return;
                 };
 
-                let clients = state.lock().await;
                 match msg {
-                    ClientMessage::Message { message, room } =>
-                        match clients.get(&room) {
-                            Some(client) => {
-                                match &client.sender {
-                                    Some(peer_tx) => peer_tx.send(ServerMessage::Message { message, room ,from: connection_id.clone()}).await.unwrap(),
-                                    None => println!("client is not available")
-                                };
-
-                            }
-                            None => {
-                                println!("room not found");
-                            }
-                        }
-                    ClientMessage::Image { bytes, message, room } => unimplemented!(),
+                    ClientMessage::Message { message, to } =>{
+                        let msg = MessageType::Text { msg: message };
+                        let msg = MessageEntry::new(connection.id.clone(), to.clone(), msg);
+                        clients.send_message(to.clone(),ServerMessage::Message(msg.clone())).await.unwrap();
+                        // response with send result
+                        send_message(socket,&ServerMessage::SendResult { id: msg.id.clone() }).await.unwrap();
+                        msg_store.add(msg).await;
+                    }
+                    ClientMessage::Disconnect => {
+                        handle_disconnect().await;
+                    },
+                    ClientMessage::Image { bytes, message, to } => unimplemented!(),
+                    ClientMessage::Read { to, id } => {
+                        clients.send_message(to, ServerMessage::SendResult { id }).await.unwrap();
+                    },
                 };
 
             }
             res = rx.recv() => {
-                if socket
-                    .send(Message::from(serde_json::to_string(&res).unwrap()))
+                if
+                    send_message(socket,&res.unwrap())
                         .await.is_err() {
                             handle_disconnect().await;
                             return;
