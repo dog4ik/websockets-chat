918ab5ac96cd232ee306314b8e30733d6ff96318
Vadim <angryvadik@gmail.com>
2023-04-23 22:42:48 +0200

    feat: Wait for support

--- client/src/pages/ClientPage.tsx
+++ client/src/pages/ClientPage.tsx
@@ -1,6 +1,5 @@
-import axios from "axios";
 import { useEffect, useRef, useState } from "react";
-import { FaArrowUp, FaSync } from "react-icons/fa";
+import { FaArrowUp, FaSpinner, FaSync } from "react-icons/fa";
 import { useSocketContext } from "../ClientSocketContext";
 import { v4 } from "uuid";
 import { ChatMessageType } from "../store";
@@ -9,6 +8,15 @@ import useConnection from "../hooks/useClientConnection";
 type MessageProps = {
   text: string;
   isMine: boolean;
+};
+const Loading = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="animate-spin">
+        <FaSpinner size={45} />
+      </div>
+    </div>
+  );
 };
 const MessageBubble = ({ text, isMine }: MessageProps) => {
   return (
@@ -27,62 +35,86 @@ const ClientPage = () => {
   const [messages, setMessages] = useState<ChatMessageType[]>([]);
   useConnection({
     onMessage(msg) {
-      setMessages((prev) => [
-        ...prev,
-        { text: msg.message, isMine: false, date: new Date() },
-      ]);
+      console.log(msg);
+      switch (msg.msg.type) {
+        case "Text": {
+          const text = msg.msg.msg;
+          setMessages((prev) => [
+            ...prev,
+            {
+              text,
+              id: msg.id,
+              isMine: false,
+              date: new Date().toString(),
+              isReaded: false,
+            },
+          ]);
+          break;
+        }
+        case "Image":
+          break;
+      }
     },
   });
-  const handleSent = () => {
+  const handleSent = async () => {
     if (message.trim() === "") return;
-    send(message);
+    let { id } = await send(message);
     setMessages((prev) => [
       ...prev,
-      { text: message, isMine: true, date: new Date() },
+      {
+        text: message,
+        id,
+        isMine: true,
+        date: new Date().toString(),
+        isReaded: true,
+      },
     ]);
     setMessage("");
   };
   useEffect(() => {
     msgContainerRef.current?.scrollTo(0, msgContainerRef.current.scrollHeight);
   }, [messages]);
+  if (!supportId) return <Loading />;
   return (
-    <div className="flex h-full items-center justify-center">
-      <div
-        ref={msgContainerRef}
-        className="flex h-96 w-64 flex-col overflow-y-auto rounded-lg bg-neutral-700"
-      >
-        <div>myId: {socketId}</div>
-        <div>supportId: {supportId}</div>
-        <div className="flex flex-1 flex-col justify-end gap-4 px-2">
-          {messages.map((msg) => (
-            <MessageBubble key={v4()} text={msg.text} isMine={msg.isMine} />
-          ))}
-        </div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSent();
-          }}
-          className="sticky bottom-0 flex rounded-lg"
+    <div>
+      <div>myId: {socketId}</div>
+      <div>supportId: {supportId}</div>
+      <div className="flex h-full items-center justify-center">
+        <div
+          ref={msgContainerRef}
+          className="flex h-96 w-64 flex-col overflow-y-auto rounded-lg bg-neutral-700"
         >
-          <input
-            className="w-full rounded-lg bg-neutral-800 px-1 outline-none"
-            value={message}
-            onChange={(e) => {
-              setMessage(e.target.value);
+          <div className="flex flex-1 flex-col justify-end gap-4 px-2">
+            {messages.map((msg) => (
+              <MessageBubble key={v4()} text={msg.text} isMine={msg.isMine} />
+            ))}
+          </div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSent();
             }}
-          />
-          <button
-            className="rounded-full bg-black p-1"
-            onClick={(e) => {
-              if (!isConnected) {
-                e.preventDefault();
-              }
-            }}
+            className="sticky bottom-0 flex rounded-lg"
           >
-            {isConnected ? <FaArrowUp size={30} /> : <FaSync size={30} />}
-          </button>
-        </form>
+            <input
+              className="w-full rounded-lg bg-neutral-800 px-1 outline-none"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+            <button
+              className="rounded-full bg-black p-1"
+              onClick={(e) => {
+                if (!isConnected) {
+                  e.preventDefault();
+                }
+              }}
+            >
+              {isConnected ? <FaArrowUp size={30} /> : <FaSync size={30} />}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
